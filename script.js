5da40ea900175fcfbc4175eb0d89a0e1371cdefa
Jeison658 <mejiabarajasjuanjose@gmail.com>
2026-06-06 21:05:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnArriba = document.getElementById("btnArriba");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        btnArriba.style.display = "block";
+    }else{
+        btnArriba.style.display = "none";
+    }
+
+});
+
+btnArriba.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
